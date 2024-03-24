2087eb5e0e245f7f514bc998f012dc782f82d981
marionjorda <marionjorda@next-u.fr>
2024-03-24 20:09:07 +0100

    To do List

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import ListeObjectifs from './components/listeObjectif';
+import AjouterObjectif from './components/ajouterObjectif';
 
-export default function App() {
+function App() {
+  const [objectifs, setObjectifs] = useState([]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{ color: 'red', fontWeight: 'bold' }}>Ma To Do List</Text>
+      <AjouterObjectif setObjectifs={setObjectifs} />
+      <ListeObjectifs objectifs={objectifs} setObjectifs={setObjectifs} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
