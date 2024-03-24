28831e84b874b109df443309c5641deeaf99235b
marionjorda <marionjorda@next-u.fr>
2024-03-24 20:27:04 +0100

    ajout objectif

---
+++ components/ajouterObjectif.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Alert, View, Button, TextInput } from 'react-native';
+
+export default function AjouterObjectif({ setObjectifs }) {
+  const [nouvelObjectif, setNouvelObjectif] = useState('');
+
+  const ajouterObjectif = () => {
+    Alert.alert(
+        'Confirmation',
+        'Voulez-vous vraiment ajouter cet objectif ?',
+        [
+          {
+            text: 'Annuler',
+            style: 'cancel',
+          },
+          {
+            text: 'Confirmer',
+            onPress: () => {
+              setObjectifs((objectifsPrecedents) => [...objectifsPrecedents, nouvelObjectif]);
+              setNouvelObjectif('');
+            },
+          },
+        ],
+        { cancelable: false },
+      );
+  };
+
+  return (
+    <View style={{ paddingTop: 50, alignItems: 'center' }}>
+      <TextInput
+        value={nouvelObjectif}
+        onChangeText={setNouvelObjectif}
+        placeholder="Nouvel objectif"
+      />
+      <Button title="Ajouter objectif" onPress={ajouterObjectif} />
+    </View>
+  );
+}
